66ca6af22c3ac813933d3e91b903c9143028bb28
Mohit Patel <mohitkanparia@gmail.com>
2019-02-06 14:32:10 -0600

    enviornment changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBnWvmmhpahKvsIlvJVMfzQQrkAj_QkzDU",
+    authDomain: "fir-demo-e987a.firebaseapp.com",
+    databaseURL: "https://fir-demo-e987a.firebaseio.com",
+    projectId: "fir-demo-e987a",
+    storageBucket: "fir-demo-e987a.appspot.com",
+    messagingSenderId: "5633456914"
+  },
 };
